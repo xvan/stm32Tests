a35a8afdafd1c6132a69fc0f947f275a32498fee
Juan Antonio Zuloaga Mellino <skirhir@gmail.com>
2023-07-12 15:07:40 -0300

    Startup

---
+++ CMakeFiles/blinker.elf.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jzuloaga/bluepill/src/main.c" "CMakeFiles/blinker.elf.dir/src/main.c.o" "gcc" "CMakeFiles/blinker.elf.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/blinker.elf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blinker.elf.dir/src/main.c.o"
+  "CMakeFiles/blinker.elf.dir/src/main.c.o.d"
+  "blinker.elf"
+  "blinker.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/blinker.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
